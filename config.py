8de7a1433d6f4645a3467c9af7507e553c70a718
Shreyansh <shreyansh@safesend.com>
2025-06-28 21:11:37 +0530

    initial setup

--- config.py
+++ config.py
@@ -12,11 +12,11 @@ logger.add(
     sys.stderr,
     colorize=True,
     format="<green>{time:MMMM-D-YYYY}</green> | \
-            <black>{time:HH:mm:ss}</black> | \
-            <level>{level}</level> | \
-            <cyan>{message}</cyan> | \
-            <magenta>{name}:{function}:{line}</magenta> | \
-            <yellow>{extra}</yellow>"
+<black>{time:HH:mm:ss}</black> | \
+<level>{level}</level> | \
+<cyan>{message}</cyan> | \
+<magenta>{name}:{function}:{line}</magenta> | \
+<yellow>{extra}</yellow>"
 )
 
 # Load environment variables from .env file
